f066fa74e7329d2287a39eb5b0e022f0d603a92f
otabek7997 <reyimbayevotabek7@gmail.com>
2025-12-15 22:46:56 +0500

    done

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,13 +4,20 @@ from rest_framework_simplejwt import (
     TokenRefreshView,
 )
 
-from .views import UserProfileView, AdminProfileView, ManagerProfileView
+from .views import (
+    UserProfileView,
+    AdminProfileView,
+    ManagerProfileView, GoogleLoginView,
+    GoogleCallbackView
+)
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/', UserProfileView.as_view(), name='user-view'),
-    path('admin/', AdminProfileView.as_view(), name='admin-view'),
-    path('manager/', ManagerProfileView.as_view(), name='manager-view')
+    path('user/', UserProfileView.as_view(), name='user_view'),
+    path('admin/', AdminProfileView.as_view(), name='admin_view'),
+    path('manager/', ManagerProfileView.as_view(), name='manager_view'),
+    path('google/login/', GoogleLoginView.as_view(), name='google_login'),
+    path('google/callback/', GoogleCallbackView.as_view(), name='google_login')
 ]
 
--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
+
+from auth_service import GoogleAuthService
 from accounts.permissions import IsAdmin, IsManager, IsUser
 
 class UserProfileView(APIView):
@@ -23,3 +26,26 @@ class AdminProfileView(APIView):
 class ManagerProfileView(APIView):
     permission_classes = [IsAuthenticated, IsManager]
     authentication_classes = [JWTAuthentication]    
+
+
+class GoogleLoginView(APIView):
+
+    def post(self, request: Request) -> Response:
+        url = GoogleAuthService.generate_google_url()
+        return Response(
+            {
+                'message': 'link to login by google',
+                'google_auth_link': url
+            }
+        )
+    
+
+class GoogleCallbackView(APIView):
+
+    def get(self, request: Request) -> Response:
+        code = request.query_params.get('code')
+        if code is None:
+            return Response({'error': 'code is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        tokens = GoogleAuthService.login_by_google(code)
+        return Response({'tokens': tokens})
